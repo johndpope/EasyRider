b32046e2062a7609a04ce0cc2f89ffd255fb2677
Nick P Smith <npsmith1990@gmail.com>
2015-10-28 22:02:02 -0500

    Something not working. Need to check cocoapod integration

---
+++ EasyRider/RideKit-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  RideKit-Bridging-Header.h
+//  EasyRider
+//
+//  Created by Nicholas Smith on 10/28/15.
+//  Copyright © 2015 Nicholas Smith. All rights reserved.
+//
+
+#ifndef RideKit_Bridging_Header_h
+#define RideKit_Bridging_Header_h
+
+#import <RideKit/RideKit.h>
+
+#endif /* RideKit_Bridging_Header_h */
